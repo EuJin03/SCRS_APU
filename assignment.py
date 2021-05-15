7f1fea57b5c921dff3960a47baadf742c77e4e15
eugene <eugenetin99@gmail.com>
2021-05-15 19:16:24 +0800

    -update: customer section partial complete

--- assignment.py
+++ assignment.py
@@ -71,7 +71,7 @@ def validation(username, password, confirm_password):
     }
   
   for user in userlist:
-    if user["username"] == username:
+    if user["username"].lower() == username.lower():
       return {
         "err": True,
         "message": "Username has been taken, please try again"
@@ -203,7 +203,7 @@ def car_details(payload, brand):
     print(f"vehicle id: {id}")
     print(f"vehicle: {brand} {model}")
     print(f"year: {year}")
-    print(f"pricing: {price}")
+    print(f"pricing: RM{price}")
     if availability:
       print(f"availability: no")
     if not availability:
@@ -214,9 +214,138 @@ def car_details(payload, brand):
         print(f"currently rented by Mr/Mrs {username}")
     print("-"*20)
     print("\n")
-    
-  input("Press Enter to quit: ")
   return
+
+
+# ADMIN INTERFACE
+
+
+# CUSTOMER INTERFACE
+def delete_user(): 
+  userlist = read_file("userlist.txt")
+
+  for i in range(len(userlist)):
+    if userlist[i]["username"] == current_user[0]["username"]:
+      del userlist[i]
+      break
+  
+  return userlist
+
+def display_user():
+  clear()
+  username = current_user[0]["username"]
+  email = current_user[0]["email"]
+
+  print("Update Personal Information\n")
+  print(f"1. Username: {username}\n2. Password\n\n0. Go Back\n")
+  return input("Which detail do you wish to update? ")
+
+def check_user(action):
+  if action == 0:
+    return 
+
+  userlist = read_file("userlist.txt")
+
+  while action == 1:
+    username = input("Enter new username: ")
+    validated = validation(username, "password", "password")
+    
+    if validated["err"]:
+      clear()
+      print(validated["message"])
+    
+    if not validated["err"]:
+      return [1, username]
+  
+  while action == 2:
+    err = False
+    clear()
+    old = input("Enter old password: ")
+    new_password = input("\nEnter new password: ")
+    new_confirm = input("Confirm new password: ")
+
+    validated = validation("username", new_password, new_confirm)
+
+    if validated["err"]:
+      clear()
+      print(validated["message"])
+
+    for user in userlist:
+      if user["username"] == current_user[0]["username"]:
+        if user["password"] != encryption(old, default_salt, 1):
+          err = True
+          clear()
+          print("Old password incorrect\n\n1. Retry\n0. Quit\n")
+          choice = input("Choice: ")
+
+          if choice == "0":
+            err = False
+
+
+    if not validated["err"] and not err:
+      return [2, new_password]
+    
+def update_user(action):
+  userlist = delete_user()
+
+  if action[0] == 1:
+    updated_user = {
+      "username": action[1], 
+      "email": current_user[0]["email"], 
+      "password": current_user[0]["password"], 
+      "wallet": current_user[0]["wallet"], 
+      "rental_history": current_user[0]["rental_history"],
+      "isAdmin": current_user[0]["isAdmin"]
+    }
+
+    userlist.append(updated_user)
+    write_file("userlist.txt", userlist)
+
+  if action[0] == 2:
+    updated_user = {
+      "username": current_user[0]["username"], 
+      "email": current_user[0]["email"], 
+      "password": encryption(action[1], default_salt, 1), 
+      "wallet": current_user[0]["wallet"], 
+      "rental_history": current_user[0]["rental_history"],
+      "isAdmin": current_user[0]["isAdmin"]
+    }
+
+    userlist.append(updated_user)
+    write_file("userlist.txt", userlist)
+
+  current_user.clear()
+  return "User Info has successfully updated, Please login again"
+
+def modify_wallet():
+  clear()
+  balance = current_user[0]["wallet"]
+  decimal_balance = "{:.2f}".format(balance)
+  print(f"Your total balance remaining: RM{decimal_balance}\n")
+  print("1. Add fund\n<Enter> to Quit\n")
+  add_fund = input("Do you wish to add fund into your account? ")
+
+  if add_fund != "1":
+    return 0
+  
+  if add_fund == "1":
+    amount = input("Enter the amount you wished to deposit: RM")
+    userlist = delete_user()
+
+    updated_user = {
+      "username": current_user[0]["username"], 
+      "email": current_user[0]["email"], 
+      "password": current_user[0]["password"], 
+      "wallet": int(current_user[0]["wallet"]) + float(amount), 
+      "rental_history": current_user[0]["rental_history"],
+      "isAdmin": current_user[0]["isAdmin"]
+    }
+
+    userlist.append(updated_user)
+    write_file("userlist.txt", userlist)
+    current_user[0] = updated_user
+    modify_wallet()
+
 
 # USER INTERFACE
 def main():
@@ -228,7 +357,7 @@ def main():
   while len(current_user) == 0:
     print('\n1. Login\n2. Register\n3. View Cars\n0. Quit')
 
-    option = input('Please select a brand: ')
+    option = input('Please select a choice: ')
     if option == "2":
       register()
 
@@ -246,10 +375,11 @@ def main():
         clear()
         break
 
-      while action["payload"] != 0:
+      while action["payload"] != "0":
         payload = int(action["payload"]) - 1
         brand = action["brand"]
         car_details(payload, brand)
+        input("Press Enter to quit: ")
         clear()
         break        
       
@@ -258,7 +388,8 @@ def main():
 
   # admin
   while len(current_user) > 0 and current_user[0]["isAdmin"]:
-    print("\nWelcome Mr/Mrs", current_user[0]["username"])
+    clear()
+    print("Welcome Mr/Mrs", current_user[0]["username"].capitalize(), "\n")
     num = input("enter a number: ")
 
     if num == "1":
@@ -266,9 +397,30 @@ def main():
 
   # customer
   while len(current_user) > 0 and not current_user[0]["isAdmin"]:
-    print("hello")
-    break
+    clear()
+    print("Welcome Mr/Mrs", current_user[0]["username"].capitalize(), "\n")
+    print('1. Rent a Car\n2. Update Personal Information\n3. Rental History\n4. Check Wallet\n\n0. Logout\n')
+    user_option = input("Please enter your choice: ")
+
+    while user_option == "4":
+      clear()
+      instruction = modify_wallet()
+
+      if instruction == 0:
+        break      
+
+    while user_option == "2":
+      personal_action = display_user()
+      if personal_action == "0":
+        break
+      user_data = check_user(int(personal_action))
+      msg = update_user(user_data)
+      clear()
+      print(msg, "\n")
+      main()
+
+    if user_option == "0":
+      break   
 
 main()
 print(current_user)
-
