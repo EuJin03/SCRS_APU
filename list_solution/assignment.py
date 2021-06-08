a2b20b42eca3408d1baff4c1b5c64fa6b9445233
eugene <eugenetin99@gmail.com>
2021-06-08 19:55:10 +0800

    -update: licensing

--- list_solution/assignment.py
+++ list_solution/assignment.py
@@ -7,12 +7,19 @@
 # TP061184
 # ASIA PACIFIC UNIVERSITY OF TECHNOLOGY AND INNOVATION
 
-# todolist
-  # assign admin
-  # forgot password
-  # limit displayed data and split it into several pages
-  # User able to manually return vehicle before deadline and extend rental date
-  # ...
+# Copyright (C) 2021 SCRS_APU Open Source Project 
+
+# Licensed under the MIT License
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at 
+ 
+#       https://choosealicense.com/licenses/mit/
+ 
+# Unless required by applicable law or agreed to in writing, software 
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and 
+# limitations under the License.
 
 # convention
 # 1. return multiple statements using list
